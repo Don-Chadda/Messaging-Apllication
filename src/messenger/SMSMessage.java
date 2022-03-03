fb2e13262b89448625e5a793f47315753cbe7940
Don-Chadda <69143593+Don-Chadda@users.noreply.github.com>
2022-03-03 19:35:43 +0000

    initial commit

---
+++ src/messenger/SMSMessage.java
@@ -0,0 +1,13 @@
+package messenger;
+
+public class SMSMessage 
+{
+    public String receiver, message;
+    
+    public SMSMessage(String receiver, String message)
+    {
+        
+        this.receiver = receiver;
+        this.message = message;
+    }
+}
